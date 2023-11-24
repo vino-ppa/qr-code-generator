1fcd259b52578a206d4c275d8822314ef4a9c179
vino-ppa <103302021+vino-ppa@users.noreply.github.com>
2023-11-24 11:00:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([{
+    message: "Give the URL: ",
+    name: "URL"
+  }])
+  .then((answers) => {
+    console.log(answers);
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qrimg.png'));
+
+    fs.writeFile("Url.txt", url, (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
